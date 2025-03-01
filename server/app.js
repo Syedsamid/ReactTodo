ae0706212a6af1c1302ef6f031efcb2059f5d2c0
Syed Samid Saud <syedsamid@Syeds-MacBook-Pro.local>
2025-03-02 01:27:41 +0530

    added

--- server/app.js
+++ server/app.js
@@ -6,7 +6,7 @@ const __filename = fileURLToPath(import.meta.url);
 const __dirname = path.dirname(__filename); 
 
 const app = express();
-const PORT = 5012;
+const PORT = 5092;
 
 
 app.use(express.static(path.join(__dirname, "dist"))); 
